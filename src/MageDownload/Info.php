5e36036dd7a3ff9bce22ab9d962bf5f9a1bca68e
Steve Robbins <steve@steverobbins.com>
2016-02-06 14:48:58 -0800

    Adding parse option to Info

--- src/MageDownload/Info.php
+++ src/MageDownload/Info.php
@@ -29,16 +29,60 @@ class Info extends AbstractMageDownload
     /**
      * Send a command to the info api
      *
-     * @param string $command
-     * @param string $id
-     * @param string $token
+     * @param string  $command
+     * @param string  $id
+     * @param string  $token
+     * @param boolean $parsed
      *
      * @return string
      */
-    public function sendCommand($command, $id, $token)
+    public function sendCommand($command, $id, $token, $parsed = false)
     {
         $this->setId($id);
         $this->setToken($token);
-        return $this->action('info', $command);
+        $response = $this->action('info', $command);
+        if ($parsed) {
+            $response = $this->parseResponse($response);
+        }
+        return $response;
+    }
+
+    /**
+     * Turn the response into something readable
+     *
+     * @param string $response
+     *
+     * @return array
+     */
+    protected function parseResponse($response)
+    {
+        $bits = preg_split('/\-{5,}/', $response);
+        if (count($bits) == 1) {
+            return array($response);
+        }
+        $headers = array();
+        foreach (preg_split('/ {2,}/', $bits[0]) as $value) {
+            $headers[] = trim($value);
+        }
+        unset($headers[0]);
+        $headCount = count($headers);
+        $rows = array();
+        foreach (explode("\n", $bits[1]) as $row) {
+            if (empty($row)) {
+                continue;
+            }
+            $row = preg_split('/ {2,}/', $row);
+            unset($row[0]);
+            if ($headCount == count($row)) {
+                $newRow = array();
+                foreach ($row as $key => $value) {
+                    $newRow[$headers[$key]] = $value;
+                }
+                $rows[] = $newRow;
+            } else {
+                $rows[] = $row;
+            }
+        }
+        return $rows;
     }
 }
